fefe55ec5d2eb048ca6d0f23f682faec7af4da77
ansgustn <mhs12107656@gmail.com>
2025-11-18 15:55:17 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by dsu_student on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
